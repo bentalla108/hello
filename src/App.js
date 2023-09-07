0cedcda43ad4f5be37ba1bc610c8349712eaa208
bentalla108 <tallambaye108@gmail.com>
2023-09-07 06:34:39 +0000

    Hey Jude !!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>hey Jude ! </p>
-       <p>hey Jude ! </p>
+       <p>hey </p>
+        <Employee name ='Jude!' role='Mileu Butteur'/>
       </header>
     </div>
   );
---
+++ src/components/Employee.js
@@ -0,0 +1,10 @@
+function Employee(props){
+    return (<>
+        <h3>Hey {props.name} !!</h3>
+        <p> {props.role}</p>
+    </>)
+
+}
+
+
+export default Employee;
